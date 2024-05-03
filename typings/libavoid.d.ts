2e3a0660268bbf163ad2bfd97570a72a0c96f76d
Soeren Schoenbrod <soeren.schoenbrod@juliahub.com>
2024-05-03 17:05:25 +0200

    feat: support hyper edge improved in wasm

--- typings/libavoid.d.ts
+++ typings/libavoid.d.ts
@@ -6,6 +6,25 @@ declare interface Point {
   y: number;
 }
 
+declare interface HyperedgeNewAndDeletedObjects {
+  get_newJunctions(index: number): JunctionRef;
+  numberOfNewJunctions: number;
+  get_deletedJunctions(index: number): JunctionRef;
+  numberOfDeletedJunctions: number;
+  get_newConnectors(index: number): ConnRef;
+  numberOfNewConnectors: number;
+  get_deletedConnectors(index: number): ConnRef;
+  numberOfDeletedConnectors: number;
+  get_changedConnectors(index: number): ConnRef;
+  numberOfChangedConnectors: number;
+}
+
+declare interface HyperedgeRerouter {
+  count(): number;
+  registerHyperedgeForRerouting(junction: JunctionRef): number;
+  newAndDeletedObjects(index: number): HyperedgeNewAndDeletedObjects;
+}
+
 declare interface Router {
   new (flags: number): Router;
 
@@ -15,9 +34,15 @@ declare interface Router {
 
   moveShape(shape: ShapeRef, newPolygon: Polygon);
   moveShape(shape: ShapeRef, xDiff: number, yDiff: number);
+  moveJunction(junction: JunctionRef, point: Point);
+  moveJunction(junction: JunctionRef, xDiff: number, yDiff: number);
   deleteShape(shape: ShapeRef);
+  deleteJunction(junction: JunctionRef);
   setRoutingParameter(parameter: number, value: number): void;
   setRoutingOption(option: number, value: boolean): void;
+
+  hyperedgeRerouter(): HyperedgeRerouter;
+  newAndDeletedObjectsFromHyperedgeImprovement(): HyperedgeNewAndDeletedObjects;
 }
 
 declare interface PolyLine {
@@ -28,15 +53,55 @@ declare interface PolyLine {
 declare interface ConnEnd {
   new (point: Point): ConnEnd;
   new (shapeRef: ShapeRef, classId: number): ConnEnd;
-  createConnEndFromJunctionRef(JunctionRef: JunctionRef, classId: number): ConnEnd;
+  createConnEndFromJunctionRef(
+    JunctionRef: JunctionRef,
+    classId: number
+  ): ConnEnd;
+}
+
+declare interface ShapeConnectionPin {
+  new (
+    shapeRef: ShapeRef,
+    portId: number,
+    xOffset: number,
+    yOffset: number,
+    proportional: boolean,
+    insideOffset: number,
+    visDirs: number | undefined
+  ): ShapeConnectionPin;
+
+  new (
+    shapeRef: ShapeRef,
+    portId: number,
+    xOffset: number,
+    yOffset: number,
+    proportional: boolean,
+    insideOffset: number
+  ): ShapeConnectionPin;
+
+  new (
+    junctionRef: JunctionRef,
+    portId: number,
+    visDirs: number
+  ): ShapeConnectionPin;
+
+  updateRelativePosition(point: Point): void;
+  updateVisibility(visDirs: number): void;
+  setExclusive(isExclusive: boolean): void;
+  isExclusive(): boolean;
+  setConnectionCost(cost: number): void;
+  position(): Point;
 }
 
 declare interface ConnRef {
   new (router: Router, srcConnEnd: ConnEnd, dstConnEnd: ConnEnd): ConnRef;
 
+  id(): number;
   displayRoute(): PolyLine;
   setSourceEndpoint(srcPoint: ConnEnd): void;
   setDestEndpoint(dstPoint: ConnEnd): void;
+  getSourceEndpoint(): ConnEnd;
+  getDestEndpoint(): ConnEnd;
   setRoutingType(type: number): void;
   setCallback(callback: (connRef: ConnRef) => void, connRef: ConnRef): void;
 
@@ -44,7 +109,7 @@ declare interface ConnRef {
   doesHateCrossings(): boolean;
 }
 
-declare interface JunctionRef {
+declare interface JunctionRef extends Obstacle {
   new (router: Router, point: Point, id?: number): JunctionRef;
 
   position(): Point;
@@ -71,6 +136,7 @@ declare interface Obstacle {
 
 declare interface ShapeRef extends Obstacle {
   new (router: Router, shapePoly: Polygon): ShapeRef;
+  transformConnectionPinPositions(type: number): void;
 }
 
 export interface Avoid {
@@ -86,6 +152,7 @@ export interface Avoid {
   Obstacle: Obstacle;
   ShapeRef: ShapeRef;
   JunctionRef: JunctionRef;
+  ShapeConnectionPin: ShapeConnectionPin;
 
   destroy(obj: any): void;
 }
